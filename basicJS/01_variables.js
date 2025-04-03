4321f09f0356e2f2fe638c8a5fdfd39d99a10360
Md Taushif raja <161483845+mrTaushifRaja@users.noreply.github.com>
2025-04-03 11:01:55 +0000

    add in comment for understand in any time

---
+++ basicJS/01_variables.js
@@ -0,0 +1,18 @@
+const customerId = 1244  // const can not be updated and redeclared in this const 
+let userEmail = "mdatushifraja084@gamil.com"
+var userPassword = "hello@123"
+userCity = "jaipur"
+
+
+//customerId = 4655 can not be used
+userEmail = "taushifraja933@gmail.com"
+userPassword = "taushif@123"
+userCity = "Goa"
+
+console.log("customerId");
+/*
+Prefer not to use var
+because if issue in block scope functoinal scope
+*/
+
+console.table([customerId,userEmail,userPassword,userCity])
