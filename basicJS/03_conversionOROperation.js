a580a84bbab01ae3f38e71325bbde09aab8310f6
Md Taushif raja <161483845+mrTaushifRaja@users.noreply.github.com>
2025-04-03 17:12:57 +0000

    Learn Operation

---
+++ basicJS/03_conversionOROperation.js
@@ -0,0 +1,68 @@
+let score = 33
+console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1 => true; 0 => false;
+//"" => false ;
+//"md taushif raja" => true;
+
+
+let someNumber = 33
+console.log(someNumber)
+let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+//**************Operations*********************
+
+let value = 55
+let negvalue = -value
+console.log(negvalue);
+
+
+//Arithmatic value
+/* console.log(2+3);
+console.log(2-3);
+console.log(2*3);
+console.log(2/3);
+console.log(2**3);
+console.log(2%3);
+console.log(2==3);
+console.log(2!=3);*/
+
+
+//two string add  
+let str1 = "Md Taushif "
+let str2 =  "Raja"
+let name = str1 + str2
+console.log("my Name =", name);
+
+//this is a combination of string and number
+/*console.log("2"+3);
+console.log(55+"3");
+console.log("2"+"3");
+console.log("1"+2+2);
+console.log(1+2+"2")*/;
+
+console.log((2+6)*5/55%88);
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
