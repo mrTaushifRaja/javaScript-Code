1f078dc09e4ac952ad86af52a1c6cb0c92143092
Md Taushif raja <161483845+mrTaushifRaja@users.noreply.github.com>
2025-04-03 11:27:13 +0000

    dataTypes

---
+++ basicJS/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) we are using nodeJS, not in browser 
+console.log(3
+    +
+    3)  // code readability should be high
+
+    console.log("md taushif raja");
+    
+
+    let name = "hitesh"
+    let age = 15
+    let isloggedIn = false
+    let state;
+
+    //number => 2to power 53
+    //bigint
+    //string => ""
+    //boolean => true/false
+    //null => standalone value 
+    //undefined => 
+    //symbol => unique
+
+
+    //object
+
+    console.log(typeof "hitesh");
+    console.log(typeof null); //object
+    
